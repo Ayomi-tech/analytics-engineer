ebbfc15b4e16f3f02ceb801b99f314e803c6fccc
Ayodeji Agunbiade <ayo.omoba1@gmail.com>
2024-11-04 14:38:28 +0100

    Removed ; issue from supplier_ids

--- dbtproject/models/staging/stg_products.sql
+++ dbtproject/models/staging/stg_products.sql
@@ -1,6 +1,26 @@
-with source as (
-    select * from {{ source('northwind', 'products') }}
+with
+    source
+    as
+    (
+        select cast(supplier_ids AS integer) as supplier_id,
+            id,
+            product_code,
+            product_name,
+            description,
+            standard_cost,
+            list_price,
+            reorder_level,
+            target_level,
+            quantity_per_unit,
+            discontinued,
+            minimum_reorder_quantity,
+            category,
+            attachments
+        FROM {{ source
+    ('northwind', 'products') }}
+    WHERE supplier_ids NOT LIKE "%;%"
 )
-select *, 
-       CURRENT_TIMESTAMP() AS ingestion_timestamp
+select *,
+    CURRENT_TIMESTAMP
+() AS ingestion_timestamp
 from source
